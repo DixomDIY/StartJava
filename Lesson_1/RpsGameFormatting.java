a53e613d1389905f30bf6f39e3a531fb0c7a11d3
Alexandr <alekkk@list.ru>
2023-12-13 22:08:35 +0500

    Выполнил ДЗ по классам

--- Lesson_1/RpsGameFormatting.java
+++ Lesson_1/RpsGameFormatting.java
@@ -5,19 +5,21 @@ public class RpsGameFormatting {
     public static void main(String[] args) throws InterruptedException {
         String rock = "R";
         String scissors = "S";
-        String paper = "P"; 
+        String paper = "P";         
+        Random r = new Random();
 
         // Ход первого игрока
         String name1 = "HEL";
         System.out.println("Ход " + name1 + ": ");
-        Random r = new Random();
         int position1 = r.nextInt(1, 100);
         String sign1 = rock;
+
         if (position1 > 66) {
             sign1 = paper;
         } else if (position1 > 33) {
             sign1 = scissors;
         } 
+
         for (int i = 0; i < 5; i++) {
             System.out.print(rock + "\r") ;
             Thread.sleep(100);
@@ -27,17 +29,19 @@ public class RpsGameFormatting {
             Thread.sleep(100);
         }
         System.out.println(sign1);
-        
+
         // Ход второго игрока
         String name2 = "WALLE";
         System.out.println("Ход " + name2 + ": ");
         int position2 = r.nextInt(1, 100);
         String sign2 = rock;
+
         if (position2 > 66) {
             sign2 = paper;
         } else if (position2 > 33) {
             sign2 = scissors;
         }
+
         for (int i = 0; i < 5; i++) {
             System.out.print(rock + "\r");
             Thread.sleep(100);
@@ -52,6 +56,7 @@ public class RpsGameFormatting {
             System.out.println("Победила дружба!");
             return;
         }
+        
         String winner = name2;
         if (sign1.equals(rock)) {
             if (sign2.equals(scissors)) {
---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    String gender = "мужской";
+    String name = "Александр";
+    double height = 160;
+    double weight = 65.5;
+    int age = 35;
+
+    void go() {
+        System.out.println(name + " идёт");
+    }
+
+    void sit() {
+        System.out.println(name + " сидит");
+    }
+
+    void run() {
+        System.out.println(name + " бежит");
+    }
+
+    void speak() {
+        System.out.println(name + " говорит");
+    }
+
+    void learnJava() {
+        System.out.println(name + " учит Java");
+    }
+}
