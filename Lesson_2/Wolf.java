3a967df4163d51ca96ec0f4da3565e1ded7c0451
Alexandr <alekkk@list.ru>
2023-12-23 10:37:33 +0500

    Выполнил ДЗ Инкапсуляция: геттеры/сеттеры

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,28 +1,72 @@
 public class Wolf {
 
-    String gender;
-    String nickname;
-    int weight;
-    int age;
-    String color;
+    private String gender;
+    private String nickname;
+    private int weight;
+    private int age;
+    private String color;
 
-    void go() {
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String setAge(int age) {
+        if (age > 8) {
+            return "Не корректный возраст";
+        }
+        this.age = age;
+        return "Указан взраст " + age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void go() {
         System.out.println(nickname + " идёт");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println(nickname + " сидит");
     }
 
-    void run() {
+    public void run() {
         System.out.println(nickname + " бежит");
     }
 
-    void howl() {
+    public void howl() {
         System.out.println(nickname + " воет");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println(nickname + " охотится");
     }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,17 +2,18 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.gender = "Мужской";
-        wolf.nickname = "Шарик";
-        wolf.weight = 20;
-        wolf.age = 5;
-        wolf.color = "Серый";
-        System.out.println("Создан объект волк:" +
-                "\nПол - " + wolf.gender +
-                "\nКличка - " + wolf.nickname +
-                "\nВес - " + wolf.weight +
-                "\nВозраст - " + wolf.age +
-                "\nЦвет - " + wolf.color);
+        wolf.setGender("Мужской"); 
+        wolf.setNickname("Шарик");
+        wolf.setWeight(20);
+        System.out.println(wolf.setAge(10));
+        System.out.println(wolf.setAge(5));
+        wolf.setColor("Серый");
+        System.out.println("Создан объект волк:\n" +
+                "Пол - " + wolf.getGender() + "\n" +
+                "Кличка - " + wolf.getNickname() + "\n" +
+                "Вес - " + wolf.getWeight() + "\n" +
+                "Возраст - " + wolf.getAge() + "\n" +
+                "Цвет - " + wolf.getColor());
         wolf.go();
         wolf.sit();
         wolf.run();
