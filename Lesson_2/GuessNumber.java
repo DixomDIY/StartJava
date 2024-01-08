3b4da521230f278322e302722d9bc9297db318e7
Alexandr <alekkk@list.ru>
2024-01-08 12:53:02 +0500

    Исправил замечания по ДЗ Lesson_2

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -5,42 +5,36 @@ public class GuessNumber {
 
     private Player player1;
     private Player player2;
+    private int endSegment = 100;
+    private Scanner scanner = new Scanner(System.in);
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    private int startInterval = 0;
-    private int endSegment = 100;
     public void play() {
-        int guessNum = startInterval + (int) (Math.random() * endSegment);
+        int guessNum = 1 + (int) (Math.random() * endSegment);
         Player currentPlayer = player1;
         currentPlayer.setNum(-1);
 
         while(guessNum != currentPlayer.getNum()) {
             currentPlayer.setNum(enterNum(currentPlayer));
             if(!isGuessed(guessNum, currentPlayer)) {
-                currentPlayer = getCurrentPlayer(currentPlayer);
+                currentPlayer = changePlayer(currentPlayer);
             }
         }
     }
 
-    private Scanner scanner = new Scanner(System.in);
     private int enterNum(Player currentPlayer) {
         System.out.print(currentPlayer.getName() + " введи число: ");
         return scanner.nextInt();
     }
 
-    private Player getCurrentPlayer(Player currentPlayer) {
-        return currentPlayer == player1 ? player2 : player1;
-    }
-
     private boolean isGuessed(int guessNum, Player currentPlayer) {
         int playerNum = currentPlayer.getNum();
-        if (playerNum <= startInterval || playerNum > endSegment) {
-            System.out.println("Загаданное число не входит в полуинтервал ("
-                    + startInterval + ", " + endSegment + "]");
+        if (playerNum <= 0 || playerNum > endSegment) {
+            System.out.println("Загаданное число не входит в полуинтервал (0," + endSegment + "]");
             return false;
         }
         if (playerNum != guessNum) {
@@ -53,5 +47,9 @@ public class GuessNumber {
         }
         System.out.println("Игрок " + currentPlayer.getName() + " выиграл. Число " + guessNum);
         return true;
+    }
+
+    private Player changePlayer(Player currentPlayer) {
+        return currentPlayer == player1 ? player2 : player1;
     }
 }
