9aa9f149f8cf650874786bae1b58c5a8dac063a6
Alexandr <alekkk@list.ru>
2024-02-08 23:27:29 +0500

    Выполнил ДЗ на тему пакеты - Lesson_3

--- src/com/startjava/lesson_2_3/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3/calculator/Calculator.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_2_3.calculator;
+
 public class Calculator {
 
     private int a;
@@ -18,10 +20,10 @@ public class Calculator {
 
     public int getB() {
         return b;
-    }
-
-    public void setSign(char sign) {
-        this.sign = sign;
+    
+}
+    public void setSign(char sign) 
+{        this.sign = sign;
     }
 
     public char getSign() {
